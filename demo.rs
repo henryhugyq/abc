6b17bd9a244dc78ad25a1d12d8999dcfaba87002
henryhugyq <89083615+henryhugyq@users.noreply.github.com>
2023-03-17 23:03:08 +0800

    Add files via upload

---
+++ demo.rs
@@ -0,0 +1,35 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+fn main(){
+    let v2 = bubble_sort(&mut vec![2,5,1,6,0,9,100]);
+    println!("冒泡排序结果为{:?}",v2);
+}
+
+fn bubble_sort(vec: &mut Vec<u32>) -> Vec<u32>{
+
+    let mut v1 = Vec::new();
+    let length = vec.len();
+    let mut j = 0;
+    
+    while j < length - 1 {
+    
+        let mut i = 0;
+        while i < (length - 1 - j) {
+        
+            if vec[i] > vec[i+1]{
+                let temp = vec[i+1];
+                vec[i+1] = vec[i];
+                vec[i] = temp;
+            }
+            
+            i += 1;
+        }
+        
+        j += 1;
+        
+        v1.push(vec[length-j]);
+    }
+    
+    v1
+}
+
